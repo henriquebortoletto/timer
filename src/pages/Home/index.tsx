77089513e98593eafa3a8c34d3ef3d53eee3bb93
Henrique Bortoletto <bortolettohenrique@gmail.com>
2024-05-28 19:43:03 -0300

    feat: criando a logica do ciclo ativo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Play } from "@phosphor-icons/react";
@@ -15,7 +16,17 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 const Home = () => {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -25,9 +36,31 @@ const Home = () => {
   });
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log("> [handleCreateNewCycle]", data);
+    const createNewCycleId = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id: createNewCycleId,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(createNewCycleId);
     reset();
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
+
+  console.log({ minutes, seconds });
 
   const task = watch("task");
   const isSubmitDisabled = !task;
@@ -71,11 +104,11 @@ const Home = () => {
       </S.Form>
 
       <S.CountDown>
-        <S.Counter>0</S.Counter>
-        <S.Counter>0</S.Counter>
+        <S.Counter>{minutes[0]}</S.Counter>
+        <S.Counter>{minutes[1]}</S.Counter>
         <S.CounterSeparator>:</S.CounterSeparator>
-        <S.Counter>0</S.Counter>
-        <S.Counter>0</S.Counter>
+        <S.Counter>{seconds[0]}</S.Counter>
+        <S.Counter>{seconds[1]}</S.Counter>
       </S.CountDown>
 
       <S.Button
